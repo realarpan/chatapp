82cc932abe90aedfde6680444f8b5d78cf802205
Arpan <164992371+realarpan@users.noreply.github.com>
2026-02-12 19:05:02 +0530

    Update client.js

--- client.js
+++ client.js
@@ -11,3 +11,12 @@ socket.on('chat message', function(msg) {
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
 });
+
+input.addEventListener("input", () => {
+  socket.emit("typing");
+});
+
+socket.on("typing", (username) => {
+  typingDiv.innerText = `${username} is typing...`;
+  setTimeout(() => typingDiv.innerText = "", 2000);
+});
